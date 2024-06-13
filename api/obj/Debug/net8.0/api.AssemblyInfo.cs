2a16eefda8aa77209fcc5aab231fbf0bfe713f98
qserena <tom@serenander.se>
2024-06-13 11:08:23 +0200

    axios.post() works from React client.

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68477a179dd7b4f1184dfb3197d802b254f67b05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+219ed261f3f694ad396d154c0b3ccfa9f614bae1")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,17 +5,20 @@ import './App.css'
 function App() {
     const API_URL = 'http://localhost:4000/'
 
-    const [formData, setFormData] = useState({
+    const emptyForm = {
         id: '',
         firstName: '',
         lastName: '',
         email: '',
         comments: '',
         isFriendly: true,
-        employment: '',
-        favColor: '',
-    })
+        birthYear: 0,
+        weight: 0,
+        employmentStatus: 0,
+        favoriteColor: 0,
+    }
 
+    const [formData, setFormData] = useState(emptyForm)
     const [employees, setEmployees] = useState([])
 
     useEffect(() => {
@@ -31,30 +34,55 @@ function App() {
                 id: elem.Id,
                 firstName: elem.FirstName,
                 lastName: elem.LastName,
-                isFriendly: elem.isFriendly,
-                employment: elem.Employment,
-                favColor: elem.FavColor,
+                email: elem.Email,
+                comments: elem.Comments,
+                isFriendly: elem.IsFriendly,
+                birthYear: elem.BirthYear,
+                weight: elem.Weight,
+                employmentStatus: elem.EmploymentStatus,
+                favoriteColor: elem.FavoriteColor,
             }))
         )
-
-        // setFormData((prevData) => ({
-        //     ...prevData,
-        //     databaseId: employee.id,
-        //     firstName: employee.first_name,
-        //     lastName: employee.last_name,
-        //     isFriendly: employee.is_friendly,
-        // }))
     }
 
     async function addClick() {
-        const data = new FormData()
-        data.append('employee', formData)
-        console.log('Add' + JSON.stringify(formData, null, 4))
-        const res = await fetch(API_URL + 'api/Employee/AddEmployee', {
-            method: 'POST',
-            body: employee,
-        })
-        const result = await res.json()
+        console.log('Add')
+        const data = {
+            Id: formData.id,
+            FirstName: formData.firstName,
+            LastName: formData.lastName,
+            Email: formData.email,
+            Comments: formData.comments,
+            IsFriendly: formData.isFriendly,
+            BirthYear: formData.birthYear,
+            Weight: formData.weight,
+            EmploymentStatus: formData.employmentStatus,
+            FavoriteColor: formData.favoriteColor,
+        }
+
+        try {
+            const response = await axios.post(
+                API_URL + 'api/Employee/AddEmployee',
+                data
+            )
+
+            console.log(response)
+        } catch (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data)
+                console.log(error.response.status)
+                console.log(error.response.headers)
+            } else if (error.request) {
+                // The request was made but no response was received
+                console.log(error.request)
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log('Error', error.message)
+            }
+            console.log(error.config)
+        }
     }
 
     function handleChange(e) {
@@ -64,10 +92,6 @@ function App() {
             ...prevFormData,
             [name]: type === 'checkbox' ? checked : value,
         }))
-    }
-
-    function handleSubmit(e) {
-        e.preventDefault()
     }
 
     const employeeList = employees.map((employee) => (
@@ -80,7 +104,7 @@ function App() {
         <div className="container">
             <h1>Test Form</h1>
             <div className="main">
-                <div className="form" onSubmit={handleSubmit}>
+                <div className="form">
                     <label htmlFor="firstName">First Name</label>
                     <input
                         type="text"
@@ -106,7 +130,7 @@ function App() {
                         checked={formData.isFriendly}
                     />
                     <div className="buttons">
-                        <button id="addButton" onClick={addClick}>
+                        <button id="addButton" onClick={() => addClick()}>
                             Add
                         </button>
                         <button id="updateButton">Update</button>
